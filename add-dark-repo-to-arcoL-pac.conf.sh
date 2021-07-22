9a2d16efd7aa0751d5952d22b2c332170d35270b
Luiz Salazar <luizsalazar87@gmail.com>
2021-07-21 19:33:07 -0500

    add awesome scripts

--- add-dark-repo-to-arcoL-pac.conf.sh
+++ add-dark-repo-to-arcoL-pac.conf.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #set -e
 
-cd $HOME/arcolinuxl-iso/archiso/airootfs/etc/
-echo $'\n\n[darklinux-repo]\nSigLevel = Optional TrustedOnly\nServer  = https://sk4g.github.io/[darklinux-repo]/x86_64' | sudo tee -a pacman.conf
-
 #[darklinux-repo]
 #SigLevel = Optional TrustedOnly
 #Server  = https://sk4g.github.io/$repo/$arch
+
+cd $HOME/arcolinuxl-iso/archiso/
+echo $'\n\n[darklinux-repo]\nSigLevel = Optional TrustedOnly\nServer  = https://sk4g.github.io/$repo/x86_64' | sudo tee -a pacman.conf
+
+cd $HOME/arcolinuxl-iso/archiso/airootfs/etc/
+echo $'\n\n[darklinux-repo]\nSigLevel = Optional TrustedOnly\nServer  = https://sk4g.github.io/$repo/x86_64' | sudo tee -a pacman.conf
+
