fa2c6ae25c4f599f5d2f2eabae18aa055c2f3015
Luiz Salazar <luizsalazar87@gmail.com>
2022-03-22 17:23:33 -0500

    rm --noconfirm

--- install-virt-manager.sh
+++ install-virt-manager.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm qemu virt-manager dnsmasq iptables-nft edk2-ovmf bridge-utils
+sudo pacman -S --needed qemu virt-manager dnsmasq iptables-nft edk2-ovmf bridge-utils
 sudo usermod -a -G libvirt $(whoami)
 
 sudo sed -i 's/#unix_sock_group\ =\ "libvirt"/unix_sock_group\ =\ "libvirt"/g' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms\ =\ "0770"/unix_sock_rw_perms\ =\ "0770"/g' /etc/libvirt/libvirtd.conf
 
-#Edit /etc/libvirt/libvirtd.conf
+# Edit /etc/libvirt/libvirtd.conf
 # Uncomment these lines
 #unix_sock_group = "libvirt"
 #unix_sock_rw_perms = "0770"
